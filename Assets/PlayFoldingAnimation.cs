a1a35f47cefa975c32053e81e7b6d23d55bae9d1
SANJIL SHRESTHA <36701200+SanjilVinsmoke@users.noreply.github.com>
2022-03-23 22:18:24 +0545

    CHnaged animatiom

---
+++ Assets/PlayFoldingAnimation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayFoldingAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+  public  void PlayFirstAnimation()
+    {
+        _animator.SetBool("Top",true);
+
+    }
+  public  void PlaySecondAnimation()
+  {
+      _animator.SetBool("Bottom",true);
+
+  }
+
+  public  void StopAnimation()
+  {
+      _animator.SetBool("Top",false);
+      
+    
+        
+  }
+  public  void Stop1Animation()
+  {
+      _animator.SetBool("Bottom",false);
+      
+    
+        
+  }
+
+}
