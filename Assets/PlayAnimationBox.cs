6f140edab3af61f5df7d23680e9c54bd83726d31
SANJIL SHRESTHA <36701200+SanjilVinsmoke@users.noreply.github.com>
2022-03-24 17:19:01 +0545

    Added box animation

---
+++ Assets/PlayAnimationBox.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayAnimationBox : MonoBehaviour
+{
+	private Animator _animator;
+
+    public Button[] Buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+      
+    }
+
+    public void PlayAnimation1()
+    {
+        _animator.SetTrigger("Box1");
+        
+        
+    }
+    public void PlayAnimation2()
+    {
+        _animator.SetTrigger("Box2");
+    }
+    public void PlayAnimation3()
+    {
+        _animator.SetTrigger("Box3");
+    }
+    
+}
